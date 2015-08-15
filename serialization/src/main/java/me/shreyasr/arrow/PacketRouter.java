d529eec7d767deaa5fe2730e41a94c94352c881d
Shreyas <skraman1999@gmail.com>
2015-08-15 08:29:19 -0700

    Constant for max packet size.

--- serialization/src/main/java/me/shreyasr/arrow/PacketRouter.java
+++ serialization/src/main/java/me/shreyasr/arrow/PacketRouter.java
@@ -2,6 +2,8 @@ package me.shreyasr.arrow;
 
 public class PacketRouter {
 
+    public static final int MAX_PACKET_SIZE = 36;
+
     public final ProjectilePacketHandler projectilePacketHandler;
 
     public PacketRouter() {
