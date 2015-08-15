c2560a35e52225ea1d0732ee91aa8d4361f38d27
Femi Olukoya <oolukoya@andrew.cmu.edu>
2015-08-15 16:19:03 -0700

    obstacle collision should also be testable

--- core/src/me/shreyasr/arrow/obstacles/PlayerObstacleCollisionDetector.java
+++ core/src/me/shreyasr/arrow/obstacles/PlayerObstacleCollisionDetector.java
@@ -4,16 +4,20 @@ import me.shreyasr.arrow.entity.BaseEntity;
 
 public final class PlayerObstacleCollisionDetector {
 
+    private static final int WIDTH = 10*4;
+    private static final int HEIGHT = 16*4;
+
     private PlayerObstacleCollisionDetector() {}
 
     public boolean hasCollided(BaseEntity player, Obstacle obstacle) {
-//        float playerLeft = player.pos.x;
-//        float playerBot = player.pos.y;
-//        float playerRight = player.pos. + player.getWidth();
-//        float playerTop = player.y + player.getHeight();
-//        return playerLeft < projectileXTip && projectileXTip < playerRight &&
-//                playerBot < projectileYTip && projectileYTip < playerTop;
-        return false;
+        float playerLeft = player.pos.x;
+        float playerBot = player.pos.y;
+        float playerRight = player.pos.x + WIDTH;
+        float playerTop = player.pos.y + HEIGHT;
+        return playerLeft < obstacle.getPosition().x + obstacle.getWidth() &&
+                playerRight > obstacle.getPosition().x &&
+                playerBot < obstacle.getPosition().y + obstacle.getHeight() &&
+                playerTop > obstacle.getPosition().y;
     }
 
 }
