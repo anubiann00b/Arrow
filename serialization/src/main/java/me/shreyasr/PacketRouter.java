0deac8d5d05eeb6e7e85db292d5d9250ad8840ec
glleung <gloria.leung@rutgers.edu>
2015-08-15 11:29:42 -0400

    added collision handler

--- serialization/src/main/java/me/shreyasr/PacketRouter.java
+++ serialization/src/main/java/me/shreyasr/PacketRouter.java
@@ -3,9 +3,12 @@ package me.shreyasr;
 public class PacketRouter {
 
     public final ProjectilePacketHandler projectilePacketHandler;
+    public final CollisionPacketHandler collisionPacketHandler;
 
     public PacketRouter() {
+
         projectilePacketHandler = new ProjectilePacketHandler();
+        collisionPacketHandler = new CollisionPacketHandler();
     }
 
     public void handleIncomingPacket(byte[] data) {
