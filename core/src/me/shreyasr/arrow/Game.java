213c236c9de8b96f837a71280a5649c111753e6a
changlingh <ch-@live.com>
2015-08-15 05:50:36 -0400

    Added camera, no exceptions yet

--- core/src/me/shreyasr/arrow/Game.java
+++ core/src/me/shreyasr/arrow/Game.java
@@ -3,6 +3,7 @@ package me.shreyasr.arrow;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.ArrayList;
@@ -15,16 +16,23 @@ public class Game extends ApplicationAdapter {
 
     SpriteBatch batch;
     List<BaseEntity> entities;
+    OrthographicCamera camera;
+    Player player;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        player = new Player();
         entities = new ArrayList<BaseEntity>();
-        entities.add(new Player());
+        entities.add(player);
+        camera = new OrthographicCamera(100, 100);
     }
 
     @Override
     public void render() {
+        float x1 = player.pos.x;
+        float y1 = player.pos.y;
+
         double delta = 1;
         if (Gdx.graphics.getFramesPerSecond() != 0)
             delta = (double) 60/Gdx.graphics.getFramesPerSecond();
@@ -32,6 +40,9 @@ public class Game extends ApplicationAdapter {
         for (BaseEntity entity : entities) {
             entity.update(delta);
         }
+
+        camera.translate(player.pos.x - x1, player.pos.y - y1);
+        camera.update();
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
