e6f448bad20e8e91d4235d7af8ac75387ab21fc9
Shreyas <skraman1999@gmail.com>
2015-08-15 16:11:11 -0700

    Upgraded EC2 instance :D.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-scp -i ../HackThePlanet/game-server-key-pair.pem server/build/libs/server-1.0.jar serialization/build/libs/serialization-1.0.jar ec2-user@52.8.127.253:/tmp/ARROW_SERVER
+scp -i ../HackThePlanet/game-server-key-pair.pem server/build/libs/server-1.0.jar serialization/build/libs/serialization-1.0.jar ec2-user@54.67.114.47:
--- desktop/src/me/shreyasr/arrow/desktop/DesktopLauncher.java
+++ desktop/src/me/shreyasr/arrow/desktop/DesktopLauncher.java
@@ -11,7 +11,7 @@ public class DesktopLauncher {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = 800;
         config.height = 600;
-        new LwjglApplication(new Game(new KeyboardPlayerInputMethod(), "52.8.127.253"), config);
+        new LwjglApplication(new Game(new KeyboardPlayerInputMethod(), "54.67.114.47"), config);
 //        new LwjglApplication(new Game(new KeyboardPlayerInputMethod(), "127.0.0.1"), config);
     }
 }
